52d413dacd0a04003fc3d37416203e831c77282c
mdasifmunshe <asif35-2872@diu.edu.bd>
2023-03-26 06:17:02 +0600

    fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { LandingPage } from "./pages";
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
+      <Route path="/portfolio" element={<LandingPage />} />
     </Routes>
   );
 };
